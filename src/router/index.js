17d40dec0e7f5e14c5b10d24f0610494c446a0a2
Sergey Chivirda <si.chivirda@gmail.com>
2020-06-10 19:37:35 +0300

    Добавил все страницы. Добавил роуты на них

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,46 @@ Vue.use(VueRouter)
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: {layout: 'main'},
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
   }
 ]
 
