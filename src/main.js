6570c6d76fbf4044d35849d05a935b8d52ebaf1a
Sergey Chivirda <si.chivirda@gmail.com>
2020-06-13 03:02:14 +0300

    Реализовал фильтр для отображения даты и времени. Закончил 4 урок

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import datefilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', datefilter)
 
 new Vue({
   router,
