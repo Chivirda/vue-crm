dbc040e17865e56ed29cae3fd30ea00d1a069821
Sergey Chivirda <si.chivirda@gmail.com>
2020-06-17 20:17:18 +0300

    Удалил линтер. Подключил Firebase, реализовал авторизацию

--- src/main.js
+++ src/main.js
@@ -8,14 +8,38 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', datefilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyBH1wEc3EiVY6kLGx8uIGYljptMKyOY1mA",
+  authDomain: "vue-crm-fc0ac.firebaseapp.com",
+  databaseURL: "https://vue-crm-fc0ac.firebaseio.com",
+  projectId: "vue-crm-fc0ac",
+  storageBucket: "vue-crm-fc0ac.appspot.com",
+  messagingSenderId: "470032414681",
+  appId: "1:470032414681:web:fff0293991f193e5e98bc8",
+  measurementId: "G-FMB1FKXME3"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
